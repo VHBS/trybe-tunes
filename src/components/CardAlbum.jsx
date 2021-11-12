1ed3923ea613257715c71497e6dd628e80ce995c
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-11-12 00:03:09 -0300

    Requisito - 6

---
+++ src/components/CardAlbum.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+export default class CardAlbum extends Component {
+  render() {
+    const { collectionName,
+      artistName,
+      collectionId,
+    } = this.props;
+    return (
+      <Link
+        data-testid={ `link-to-album-${collectionId}` }
+        to={ `/album/${collectionId}` }
+      >
+        <div>
+          <h3>
+            Nome do Album:
+            {' '}
+            { collectionName }
+          </h3>
+          <p>
+            Artista:
+            {' '}
+            { artistName }
+          </p>
+        </div>
+      </Link>
+    );
+  }
+}
+
+CardAlbum.propTypes = {
+  collectionName: PropTypes.string.isRequired,
+  artistName: PropTypes.string.isRequired,
+  collectionId: PropTypes.number.isRequired,
+};
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import CardAlbum from '../components/CardAlbum';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from './Carregando';
 
 export default class Search extends Component {
   constructor() {
     super();
-
     this.state = {
+      searchResult: [],
+      loading: false,
       inputSearch: '',
       checkLength: true,
     };
     this.checkInputLength = this.checkInputLength.bind(this);
   }
 
-  checkInputLength({ target: { value } }) {
+  async checkInputLength({ target: { value } }) {
     const minVal = 2;
     const checkInput = value.length < minVal;
     this.setState({
@@ -21,28 +25,57 @@ export default class Search extends Component {
     });
   }
 
+  async callApi() {
+    const { inputSearch } = this.state;
+    this.setState({
+      loading: true,
+    });
+    const doidera = await searchAlbumsAPI(inputSearch);
+    this.setState({
+      loading: false,
+      searchResult: [...doidera],
+    });
+  }
+
   render() {
-    const { inputSearch, checkLength } = this.state;
+    const { inputSearch, checkLength, loading, searchResult } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <label htmlFor="artist-input">
-          Procurar por banda ou artista:
-          <input
-            data-testid="search-artist-input"
-            type="text"
-            id="artist-input"
-            onChange={ this.checkInputLength }
-          />
-        </label>
-        <p>{ inputSearch }</p>
-        <button
-          data-testid="search-artist-button"
-          type="button"
-          disabled={ checkLength }
-        >
-          Pesquisar
-        </button>
+        {loading ? (<Carregando />)
+          : (
+            <div>
+              <label htmlFor="artist-input">
+                Procurar por banda ou artista:
+                <input
+                  data-testid="search-artist-input"
+                  type="text"
+                  id="artist-input"
+                  onChange={ this.checkInputLength }
+                />
+              </label>
+              <h4>
+                Resultado de álbuns de:
+                {' '}
+                { inputSearch }
+              </h4>
+              {searchResult.length > 0 ? (searchResult.map((album) => (
+                <div key={ album.collectionId }>
+                  <CardAlbum
+                    { ... album }
+                  />
+                </div>
+              ))) : (<p>Nenhum álbum foi encontrado</p>)}
+              <button
+                data-testid="search-artist-button"
+                type="button"
+                disabled={ checkLength }
+                onClick={ async () => this.callApi() }
+              >
+                Pesquisar
+              </button>
+            </div>
+          )}
       </div>
     );
   }
