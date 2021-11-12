d784e9ba4036cbef529ca464a51a28e169f004d4
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-11-11 22:35:55 -0300

    Requisito - 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Carregando from '../pages/Carregando';
 import { getUser } from '../services/userAPI';
 
@@ -31,6 +32,15 @@ export default class Header extends Component {
         ? (<Carregando />)
         : (
           <header data-testid="header-component">
+            <Link data-testid="link-to-search" to="/search">
+              Search
+            </Link>
+            <Link data-testid="link-to-favorites" to="/favorites">
+              Favorites
+            </Link>
+            <Link data-testid="link-to-profile" to="/profile">
+              Profile
+            </Link>
             <h1 data-testid="header-user-name">{ name }</h1>
           </header>
         )
